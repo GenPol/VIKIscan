0477d34ea5795646daaa3dfd899ea91b396b37a3
Gen Pol <gariwood@ya.ru>
2023-11-19 23:37:45 +0300

    Update Подсчёт Товара.py

--- Подсчёт Товара.py
+++ Подсчёт Товара.py
@@ -1,21 +1,22 @@
 import pandas as pd
-
 import warnings
+
 warnings.simplefilter("ignore")
 
-
 # Читаем файл Excel
-data = pd.read_excel(r"C:\Users\user1c\Desktop\python project\18nov.xlsx")
+data = pd.read_excel(r"18nov.xlsx")
 
 # Извлекаем колонку 'column_name' из данных
-prices = data['Сумма безналичными']
-checes = data['Чек']
-
-priceList = [i for i in prices]
-checkList = [i for i in checes]
-
-dictFinal = {}
+beznal = data['Сумма безналичными']
+checks = data['Чек']
 
 
-# Если вы хотите сохранить данные в списке, вы можете сделать так:
-#list_data = column.tolist()
+def tocheck(sums, nums):
+    # convert to list data
+    SumList = [i for i in sums]
+    NumList = [i for i in nums]
+    return SumList
+
+
+
+print(tocheck(beznal, checks))
